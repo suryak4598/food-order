397dc70efe41bac3c3435e5c3800eb27469ac8e6
Vijay <${env.BT_GIT_USER}@worksap.co.jp>
2022-06-29 17:15:57 +0530

    initial commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import classes from "./Header.module.css";
+import mealsImage from "../../asset/banner.jpg"
+import HeaderCart from "./HeaderCart";
+
+const Header = () => {
+
+    return(
+        <React.Fragment>
+        <header className={classes.header}>
+            <h1>Order Your Food</h1>
+            <h1>Your Food</h1>
+            {/* <HeaderCart/> */}
+        </header>
+        <div className={classes['background']}>
+            <img src={mealsImage} alt='background' />
+        </div>
+      </React.Fragment>
+    );
+
+}
+
+export default Header;
---
+++ src/components/Layout/HeaderCart.js
@@ -0,0 +1,16 @@
+import classes from "./HeaderCart.module.css";
+
+
+
+
+const HeaderCart = () => {
+
+
+    return(
+    <div className={classes.header__cart}>
+        <h3>Your Cart</h3>
+    </div>)
+
+}
+
+export default HeaderCart;
